8319f985cb0d4c8134b11bae2299ebd4c38f75cc
ViniRodrig <vmarquesrodrigues25@gmail.com>
2024-09-21 23:28:18 -0300

    aaa

--- eth10M.py
+++ eth10M.py
@@ -1,5 +1,23 @@
 def decode(sig):
-    # TODO: processe o sinal sig (uma lista de inteiros 0 ou 1) e retorne
-    # o quadro Ethernet como um valor do tipo bytes, contendo desde o
-    # endereço MAC de destino até o FCS.
-    return bytes([])
+    out = []
+    counter = 0
+    for i in range(len(sig)):
+        counter += 1
+        if sig[i-1] != sig[i] and counter >= 7:
+            out.append(sig[i])
+            counter = 0
+    preamble = True
+    byte_reset = 0b1_0000_0000
+    byte = byte_reset
+    frame = []
+
+    for i in range(len(out)):
+        if preamble and out[i-1:i+1] == [1, 1]:
+            preamble = False
+        elif not preamble:
+            byte = byte >> 1 | out[i] << 8
+            if byte & 1 == 1:
+                frame.append(byte >> 1)
+                byte = byte_reset
+
+    return bytes(frame)
